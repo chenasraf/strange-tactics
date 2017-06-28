8ebb45c0370acf2e71d65bbf7950991c8549a84d
Chen Asraf <chen@asraf.me>
2017-06-28 23:29:23 +0300

    Major refactor, input handler overhaul

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path')
+
+module.exports = {
+  entry: './lib/game.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: "source-map",
+  // module: {
+  //   rules: [{
+  //     test: /\.js$/,
+  //     exclude: /(node_modules|bower_components)/,
+  //     use: {
+  //       loader: 'babel-loader',
+  //       options: {
+  //         presets: ['env', 'es2016']
+  //       }
+  //     }
+  //   }]
+  // }
+}
